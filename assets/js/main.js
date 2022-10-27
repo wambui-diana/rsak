99c656fbcdbebf34f9a1f47a582574be75e3ed45
wambui-diana <muchohidiana@gmail.com>
2022-10-27 11:35:52 +0300

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -258,17 +258,3 @@
   });
 
 })()
-
-$(document).ready(function() {
-    $('.counter').each(function () {
-      $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-      }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-      });
-    }); 
-});  
